4f835900c884929f227600cb2ff85d6af5186e40
Aderibigbe Michael Oloruntomiwa <107891405+moloruntomiwa31@users.noreply.github.com>
2024-04-27 11:59:25 +0100

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <footer class="bg-[#9a031e] md:min-h-[150px] w-full flex items-center justify-center container-pd">
+  <footer
+    class="bg-[#9a031e] md:min-h-[150px] w-full flex items-center justify-center container-pd"
+  >
     <div class="md:w-1/4">
       <img
         src="../assets/images/app-logo.png"
@@ -8,8 +10,14 @@
       />
     </div>
     <div class="grid grid-cols-2 md:grid-cols-3 md:w-3/4">
-      <div class="grid">
-        <RouterLink :to="link.to" v-for="link in footerLinks">{{ link.name }}</RouterLink>
+      <div class="grid gap-4">
+        <RouterLink :to="link.to" v-for="link in footerLinks">{{
+          link.name
+        }}</RouterLink>
+        <a
+          href="https://public.tableau.com/views/PREDICTIONDATASET/Dashboard3?:language=en-GB&publish=yes&:sid=CD18AA06EECF4C5483AB6FA9B0286977-0:0&:display_count=n&:origin=viz_share_link"
+          >Tableau Visualization</a
+        >
         <a href="#">NewsLetter</a>
       </div>
       <div class="grid">
@@ -20,7 +28,10 @@
         <h4 class="text-white">
           Want to receive the latest from our Newsletter?
         </h4>
-        <Input placeholder="Enter your email..." class="bg-transparent placeholder:text-white text-white" />
+        <Input
+          placeholder="Enter your email..."
+          class="bg-transparent placeholder:text-white text-white"
+        />
       </div>
     </div>
   </footer>
